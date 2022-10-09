69dbd2c79f7289025d604c6d790e26fc68273ff8
Skander Bennia <skander.bennia@avaxia-group.com>
2022-10-09 17:53:08 +0100

    expediteur navbar permissions

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,50 @@
 import { Button, Layout, Menu } from "antd";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+import { userAtom } from "../../atoms/userAtom";
 import React from "react";
 import Logo from "../../assets/images/logo.png";
 
 const { Header, Content } = Layout;
 const Navbar = (props) => {
+  const user = useRecoilValue(userAtom);
+  const expediteurRoute = [
+    {
+      key: "1",
+      label: "Bordreau",
+      style: { fontWeight: "bold" },
+      onClick: () => {
+        router.push("/dashboard/bordreau");
+      },
+    },
+  ];
+  const adminRoute = [
+    {
+      key: "1",
+      label: "Dashboard",
+      style: { fontWeight: "bold" },
+      onClick: () => {
+        router.push("/dashboard/bordreau");
+      },
+    },
+    {
+      key: "2",
+      label: "Bordereau",
+      style: { fontWeight: "bold" },
+      onClick: () => {
+        router.push("/dashboard/bordreau");
+      },
+    },
+    {
+      key: "3",
+      label: "Livreur",
+      style: { fontWeight: "bold" },
+      onClick: () => {
+        router.push("/dashboard/bordreau");
+      },
+    },
+  ];
   const router = useRouter();
   return (
     <Layout className="layout">
@@ -29,24 +68,7 @@ const Navbar = (props) => {
           style={{ background: "white", width: "100%" }}
           mode="horizontal"
           defaultSelectedKeys={["0"]}
-          items={[
-            {
-              key: "1",
-              label: "Bordreau",
-              style: { fontWeight: "bold" },
-              onClick: () => {
-                router.push("/dashboard/bordreau");
-              },
-            },
-            {
-              key: "2",
-              label: "Livreur",
-              style: { fontWeight: "bold" },
-              onClick: () => {
-                router.push("/dashboard/bordreau");
-              },
-            },
-          ]}
+          items={user.role === "admin" ? adminRoute : expediteurRoute}
         />
         <Button
           type="default"
