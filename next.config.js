155f434c75385c4d6b62ae2b5c796d2503462efc
OussamaChatti <oussama.chatti.12@gmail.com>
2022-11-08 15:20:26 +0100

    added search robots index for vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 }
 
 module.exports = nextConfig
